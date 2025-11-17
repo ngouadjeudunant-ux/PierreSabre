35d07ffa7b8b9c67f799cbb1a784c6d1364f6727
Dunant <ngouadjeudunant@gmail.com>
2025-11-17 17:46:04 +0100

    debut classe Yakuza

---
+++ src/personnages/Yakuza.java
@@ -0,0 +1,13 @@
+package personnages;
+
+public class Yakuza extends Humain{
+	
+	private String clan; 
+	private int reputation = 0; 
+	
+	public Yakuza(String nom, String boissonPreferee, int argent, String reputation) {
+		super(nom, boissonPreferee, argent); 
+		this.clan = clan; 
+	}
+
+}
